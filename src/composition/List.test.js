a181b2f12de3ca8f413aedeb4fd679d74c3ff11b
Drew Love <davidandrewlove84@gmail.com>
2019-02-09 10:29:28 -0500

    List test

--- src/composition/List.test.js
+++ src/composition/List.test.js
@@ -1,12 +1,24 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
+import renderer from 'react-test-renderer'
 import List from './List'; 
 import STORE from '../store'; 
 
-it('renders correctly', ()=> {
-    const div = document.createElement('div'); 
-     
-
-    ReactDOM.render(<List cards = {cards} />, div); 
-    ReactDOM.unmountComponentAtNode(div)
+describe('List tests', () => {
+    it('List component: renders without crashing', ()=> {
+        const div = document.createElement('div'); 
+        let cards=STORE.lists[0].cardIds.map(id => STORE.allCards[id])
+    
+        ReactDOM.render(<List key='1' header='2' cards={cards}/>, div); 
+        ReactDOM.unmountComponentAtNode(div)
+    })
+    
+    it('List component: renders UI correctly', ()=> {
+        let cards=STORE.lists[0].cardIds.map(id => STORE.allCards[id])
+        const tree = renderer
+        .create(<List key='1' header='2' cards={cards}/>)
+        .toJSON(); 
+        expect(tree).toMatchSnapshot()
+    })
 })
+
